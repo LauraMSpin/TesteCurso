1aa899484d59a8b7a65cb410828e8d8b7b9ab328
Laura Mendes <lauramendespin@gmail.com>
2020-06-16 16:47:10 -0300

    Acrescentada mensagem bom dia

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -4,6 +4,7 @@ namespace TesteGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola Mundo");
+            Console.WriteLine("Bom Dia");
 
             Console.ReadLine();
         }
