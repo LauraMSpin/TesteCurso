3f66f30cfe32ea230f1e07423f1203e47b02cec0
Laura Mendes <lauramendespin@gmail.com>
2020-06-16 16:45:36 -0300

    Projeto teste iniciado ola mundo

---
+++ TesteGit/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TesteGit {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ola Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
